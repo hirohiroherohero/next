9c90698d782a1df1695774738d4b375cecfe3c6f
hiro <hirohiroherohero93@gmail.com>
2023-08-02 11:42:59 +0900

    feat: axios instance

---
+++ src/utils/apiClient/index.ts
@@ -0,0 +1,44 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
+const baseInstanceInfo: AxiosRequestConfig = {
+  baseURL: '',
+  headers: {
+    //   Authorization: `Bearer `,
+  },
+  withCredentials: false,
+};
+
+const instance = axios.create(baseInstanceInfo);
+
+instance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export const axiosGet = (url: string) => {
+  return instance.get(url);
+};
+
+export const axiosPost = (url: string, config: AxiosRequestConfig) => {
+  return instance.post(url, config);
+};
+
+export const axiosPut = (url: string, config: AxiosRequestConfig) => {
+  return instance.put(url, config);
+};
+
+export const axiosPatch = (url: string, config: AxiosRequestConfig) => {
+  return instance.patch(url, config);
+};
