b7c11d9dc05117e2b268f7c0eef52ebb38720ff6
hiro <hirohiroherohero93@gmail.com>
2023-08-08 16:48:38 +0900

    feat: utils types RecursiveInferArray

--- src/utils/types/index.d.ts
+++ src/utils/types/index.d.ts
@@ -1,1 +1,5 @@
 type InferArray<T extends unknown[]> = T extends (infer U)[] ? U : never;
+
+type RecursiveInferArray<T extends unknown[]> = T extends (infer U)[]
+  ? { [key in keyof U]: U[key] extends unknown[] ? RecursiveInferArray<U[key]> : U[key] }
+  : never;
